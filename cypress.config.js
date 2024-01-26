a131c4deaf72f7a6150acba07087a04b1cd57784
sajidbashir123 <sajidbashir278@gmail.com>
2024-01-27 00:58:24 +0500

    delete extra file of json/html file

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,19 @@ module.exports = defineConfig({
   projectId: "jw13m9",
   e2e: {
     baseUrl: "https://dev.meltzerhellrung.com",
-    video: true,
-    screenshotOnRunFailure: true,
+    video: false,
     watchForFileChanges: false, // Not run automatically after any file changes
     defaultCommandTimeout: 20000,
     reporter: "mochawesome", // To generate the HTML report of the executed tested case for external use. it added when we install the mochaawesome dependency
     reporterOptions: {
       reportDir: "cypress/results",
-      charts: true,
+      charts: false,
       overwrite: false,
       html: true,
       json: true,
     },
-    screenshotOnRunFailure: true,
-    screenshotsFolder: "cypress/screenshots",
+    screenshotOnRunFailure: false,
+    //screenshotsFolder: "cypress/screenshots",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
