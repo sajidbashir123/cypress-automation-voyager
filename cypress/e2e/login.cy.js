052178d4f8db84b9f3a15b66ba612547d903344c
sajidbashir123 <sajidbashir278@gmail.com>
2024-02-23 00:25:47 +0500

    Added different methods of Pageobject model for better code readbility and resuability

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,55 @@
+import Login from "./PageObject/login1";
+import {Login as Logindupliation} from "./PageObject/login2";
 /// <reference types="Cypress" />
 
+
 describe("New Process Request Creation", () => {
-  it("login", () => {
+
+  //*************** General Login*****************/
+
+  it("General hardcoded login", () => {
+   
     cy.visit("/"); // The base url is set in "cypress.config.js" file and we can use it globally
     //login
     cy.get("#username-input").type("sajid.bashir+mh3@kwanso.com");
     cy.get("#password-input").type("Test#123{Enter}");
     cy.wait(5000);
-    cy.get("#header-avatar").click();
-    cy.wait(1000);
-    cy.get("#header-sign-out").click();
-    cy.wait(4000);
   });
+
+  //***************Login with Class and Object and geting data by making objects - login1.js*****************/
+
+  it.only("login making class and object", () => {
+
+    cy.visit("/");  
+    const login= new Login
+    login.setUserName("sajid.bashir+mh3@kwanso.com");
+    login.setPassword("Test#123");
+    login.clickSingIn();
+  });
+
+ //***************Login with Class and Object and geting data by making varables - login2.js*****************/
+
+  it("login making class and object", () => {
+
+    cy.visit("/");  
+    const login= new Logindupliation
+    login.setUserName("sajid.bashir+mh3@kwanso.com");
+    login.setPassword("Test#123");
+    login.clickSingIn();
+  });
+
+ //***************Login with Class and Object and geting data from the fixture file*****************/
+
+  it("login by fetching data from the fixture file", () => {
+    cy.visit("/");
+
+    cy.fixture("login").then((data)=>{
+
+     const login= new Login
+     login.setUserName(data.username);
+     login.setPassword(data.password);
+     login.clickSingIn();
+    });
+  });
+
 });
